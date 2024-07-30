d18946b0cda5bcd11fe857829237d2986d71dbf6
unknown <andy.luarte@gmail.com>
2024-07-30 18:06:21 -0400

    prueba 22 dashboard

--- agrosmartiotweb/views.py
+++ agrosmartiotweb/views.py
@@ -665,29 +665,13 @@ def user_logout(request):
     auth.logout(request)
     return redirect ("home")
 
-# def FinanzasList(request):
-#     finanzas = Finanzas.objects.all()
-#     finanzas_por_mes = FinanzasPorMes.objects.all()
-#     gastos_por_mes = GastoFinanciero.objects.gastos_por_mes()
-    
-#     return render(request, 'agrosmart/finanzas/gestion_finanzas.html', {'finanzas': finanzas,'finanzas_por_mes': finanzas_por_mes,'gastos_por_mes':gastos_por_mes})
-
 def FinanzasList(request):
+    finanzas = Finanzas.objects.all()
     finanzas_por_mes = FinanzasPorMes.objects.all()
+    gastos_por_mes = GastoFinanciero.objects.gastos_por_mes()
+    
+    return render(request, 'agrosmart/finanzas/gestion_finanzas.html', {'finanzas': finanzas,'finanzas_por_mes': finanzas_por_mes,'gastos_por_mes':gastos_por_mes})
 
-    # Preparar datos para el gráfico
-    finanzas_data = [
-        {
-            'mes': entry.mes,
-            'total_gastos': float(entry.total_gastos_en_remuneracion)
-        } for entry in finanzas_por_mes
-    ]
-
-    context = {
-        'finanzas_data': json.dumps(finanzas_data),
-    }
-
-    return render(request, 'agrosmart/finanzas/gestion_finanzas.html', context)
 ###gasto financiero 
 def agregar_gasto_financiero(request):
     if request.method == 'POST':
